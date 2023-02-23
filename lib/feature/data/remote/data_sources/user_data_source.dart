7a22f506f1ca299f84ad225ef6a3b1addf657722
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-02-23 19:46:01 +0600

    v-2.1.3 [api]

--- lib/feature/data/remote/data_sources/user_data_source.dart
+++ lib/feature/data/remote/data_sources/user_data_source.dart
@@ -2,8 +2,6 @@ import 'package:flutter_communication/contents.dart';
 import 'package:flutter_communication/core/common/data_sources/realtime_data_source.dart';
 import 'package:flutter_communication/feature/domain/entities/user_entity.dart';
 
-import '../../../../core/common/data_sources/fire_store_data_source.dart';
-
 class UserDataSource extends RealtimeDataSource<UserEntity> {
   UserDataSource({
     super.path = ApiPaths.users,
